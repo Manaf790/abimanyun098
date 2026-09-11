e443e069d470ca182d7cd1982d03ee614b2b4531
Manaf790 <blsmk.260127034@budiluhur.sch.id>
2026-09-11 11:17:03 +0700

    Add files via upload

--- phiton.py
+++ phiton.py
@@ -72,16 +72,111 @@
 
 
 
-#rows = 5
-#for i in range(1, rows + 1):
+# rows = 5
+# for i in range(1, rows + 1):
 #  print(" " * (rows -i) + "*" * (2 * i - 1))
 
 
 
-#rows = 5
-#while rows >= 1:
+# rows = 5
+# while rows >= 1:
 #    print("*" * rows)
 #    rows -= 1
 
+
+
+rows = 5
+i = 1
+while i <= rows:
+    print(" " * (rows - i) + "*" * i)
+    i += 1
+
+
+
+#def luaspersegi(sisi):
+#    return sisi * sisi
+
+#print(luaspersegi(4))
+
+
+#def ganjilgenap(angka):
+#    if angka % 2 == 0:
+#        return "Genap"
+#    else:
+#        return "Ganjil"
+
+#print(("Cek Ganjil Genap: "), ganjilgenap (6))
+#print(("Cek Ganjil Genap: "), ganjilgenap (7))
+
+
+
+#def konversi_suhu(celcius):
+#    fahrenheit = (celcius * 1.8) + 32
+#    return fahrenheit
+#
+#print(("Konversi Suhu: "), konversi_suhu(20),"Fahrenheit")
+
+
+
+#nama = input("Masukkan nama: ")
+#umur = int(input("Masukkan umur: "))
+
+#print(f"Halo {nama}, umur kamu {umur} tahun.")
+
+
+
+# def hitungTabungan(hari):
+#    total = 0
+#    for h in range(1, hari + 1):
+#        if h%2!=0:
+#            total += 2000
+#        else:
+#            total += 5000
+
+#    return total
+
+# print(hitungTabungan(3))
+
+
+
+# def cekTilang(plat, tanggal):
+#    if plat % 2 == 0 and tanggal % 2 == 0:
+#        return "Anda Aman."
+#    elif plat % 2 == 1 and tanggal % 2 == 1:
+#            return "Anda Aman."
+#    elif plat % 2 == 1 and tanggal % 2 == 0:
+#        return "Anda Ditilang."
+#    elif plat % 2 == 0 and tanggal % 2 == 1:
+#            return "Anda Ditilang."
+
+# print(cekTilang(24, 12))
+# print(cekTilang(13, 20))
+
+
+
+# def hitungKopi(jumlahGelas):
+#    hargaGelas = 5000
+#    totalHarga = jumlahGelas * hargaGelas
+
+#    if jumlahGelas >= 3:
+#        totalHarga -= totalHarga * 0.1 
+
+#    return totalHarga
+
+
+# print(hitungKopi(2))
+# print(hitungKopi(4))
+
+
+
+# def tepokNyamuk(jumlah):
+#     for angka in range(1, jumlah + 1):
+#         if angka % 3 == 0:
+#             print("TEPOK")
+#         else:
+#             print(angka)
+
+# print(tepokNyamuk(5))
+    
 
 
